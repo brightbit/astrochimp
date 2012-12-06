6f9a5f9af289c12e11e56e9f18daa39e57624279
Jonathan Camenisch <jonathan@camenisch.net>
2012-12-06 12:33:05 -0600

    Catch up dummy app's schema file

--- test/dummy/db/schema.rb
+++ test/dummy/db/schema.rb
@@ -11,13 +11,19 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20121025202343) do
+ActiveRecord::Schema.define(:version => 20121101211218) do
 
   create_table "astrochimp_signups", :force => true do |t|
     t.string   "email"
     t.integer  "status"
-    t.datetime "created_at", :null => false
-    t.datetime "updated_at", :null => false
+    t.datetime "created_at",                    :null => false
+    t.datetime "updated_at",                    :null => false
+    t.integer  "referral_id"
+    t.string   "referral_code"
+    t.integer  "referral_count", :default => 0
   end
+
+  add_index "astrochimp_signups", ["referral_code"], :name => "index_astrochimp_signups_on_referral_code"
+  add_index "astrochimp_signups", ["referral_id"], :name => "index_astrochimp_signups_on_referral_id"
 
 end
