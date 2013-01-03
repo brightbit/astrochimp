fd313bfad836244859394bcec65e310991444d60
Jonathan Camenisch <jonathan@camenisch.net>
2013-01-03 14:14:08 -0600

    Namespace url for referral links

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,5 @@ Astrochimp::Engine.routes.draw do
   root to: "signups#index"
 
   resources :signups, only: [:index, :create, :show]
-  get ':referrer_code' => 'signups#index', as: :referral_link
+  get 'r/:referrer_code' => 'signups#index', as: :referral_link
 end
